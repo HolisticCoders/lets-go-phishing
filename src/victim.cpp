442dbda65e1b8c4a0656c1af5151a6127621a830
Aphosis <alexy.long@orange.fr>
2018-10-06 18:17:59 +0200

    Updated Victim constructor.

--- src/victim.cpp
+++ src/victim.cpp
@@ -6,20 +6,29 @@ using namespace std;
 
 class Victim {
     public:
-        Victim(const string& name, const string& avatarUrl, const string& bio, const Professions& profession, const MaritalStatus& maritalStatus, const int& children, const int& money) {
-            m_name = name;
-            m_avatarUrl = avatarUrl;
-            m_bio = bio;
-            m_maritalStatus = maritalStatus;
-            m_children = children;
-            m_money = money;
+        Victim() {
+            m_name = "Person";
+            m_avatarUrl = "";
+            m_bio = "";
+            m_profession = Teacher;
+            m_maritalStatus = Single;
+            m_children = 0;
+            m_money = 0;
         }
+        Victim(
+                const string& name, const string& avatarUrl, const string& bio,
+                const Professions& profession, const MaritalStatus& maritalStatus,
+                const int& children, const int& money)
+            : m_name{name}, m_avatarUrl{avatarUrl}, m_bio{bio},
+            m_profession(profession), m_maritalStatus{maritalStatus},
+            m_children{children}, m_money{money}
+        {}
     private:
         string m_name;
         string m_avatarUrl;
         string m_bio;
         Professions m_profession;
         MaritalStatus m_maritalStatus;
-        unsigned int m_children;
+        int m_children;
         int m_money;
 };
