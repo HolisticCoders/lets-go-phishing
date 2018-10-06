13400a776a1d2478c78b24c889b3c219cbe2c42b
Aphosis <alexy.long@orange.fr>
2018-10-06 18:50:11 +0200

    Added GUI Board class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,29 @@
 #include "raylib.h"
 
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
-
-#include "player.cpp"
 #include "mail.cpp"
+#include "ui/board.cpp"
 
 
 int main() {
 
-    const int screenWidth = 960;
-    const int screenHeight = 540;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "Let's go phishing!");
 
     SetTargetFPS(30);
 
     Player player;
+    Board board;
+    board.setPlayer(&player);
 
-    // Initialize the spam e-mails.
-    constexpr unsigned int mailCount = 4;
-    Mail mails[mailCount];
-    mails[0].setTitle("What a deal!");
-    mails[0].setContent("Hesitate no longer, this awesome gaming PC should be yours today!");
-    mails[0].setCategory(Products);
-    mails[1].setTitle("Have you met Ted ?");
-    mails[1].setContent("Lonely, handsome singles in the bar next door!");
-    mails[1].setCategory(Dating);
-    mails[2].setTitle("Cross the street and get a job!");
-    mails[2].setContent("Long past are the days of boredom and poverty, follow my advice and get a job fitting your skills today!");
-    mails[2].setCategory(Jobs);
-    mails[3].setTitle("Don't let the virus spread!");
-    mails[3].setContent("PLEASE READ THIS E-MAIL CAREFULLY!!! The ebola virus is coming back, you MUST take this medecine or risk being exposed and DIE!!!");
-    mails[3].setCategory(HealthCare);
-
-    while (!WindowShouldClose() && player.wantedLevel() < 100) {
+    while (!WindowShouldClose() || !board.closing()) {
 
         BeginDrawing();
 
-            ClearBackground(BLACK);
+            ClearBackground(GetColor(style[DEFAULT_BACKGROUND_COLOR]));
+
+            board.update();
 
         EndDrawing();
     }
---
+++ src/ui/board.cpp
@@ -0,0 +1,59 @@
+#include <string>
+
+#include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+#include "../player.cpp"
+
+
+class Board {
+    public:
+
+        // Constructors
+        Board() {
+            m_player = nullptr;
+        }
+        Board(Player* player) : m_player{player} {}
+
+        // Accessors.
+        Player* player() {
+            return m_player;
+        }
+        bool closing() {
+            return m_closing;
+        }
+
+        // Mutators
+        void setPlayer(Player* player) {
+            m_player = player;
+        }
+        void setClosing(const bool& closing) {
+            m_closing = closing;
+        }
+
+        // Member functions
+        void update() {
+            if (GuiWindowBox((Rectangle){ 0, 0, 1280, 720 }, "Let's go phishing!")) {
+                setClosing(true);
+            }
+            GuiGroupBox((Rectangle){ 925, 50, 330, 550 }, "Twatter");
+            GuiGroupBox((Rectangle){ 475, 50, 425, 645 }, "Profile");
+            GuiGroupBox((Rectangle){ 25, 50, 425, 570 }, "Send");
+            GuiGroupBox((Rectangle){ 25, 645, 425, 50 }, "Emails");
+            GuiGroupBox((Rectangle){ 925, 625, 330, 70 }, "Stats");
+
+            GuiLabel((Rectangle){ 935, 635, 36, 25 }, "Money:");
+            GuiLabel((Rectangle){ 935, 655, 71, 25 }, "Wanted level:");
+
+            GuiSliderBar((Rectangle){ 1045, 660, 185, 15 }, m_player->wantedLevel(), 0, 100);
+
+            const char* money = std::to_string(m_player->money()).c_str();
+            GuiLabel((Rectangle){ 1045, 635, 161, 25 }, money);
+        }
+
+    private:
+        Player* m_player;
+        bool m_closing = false;
+};
