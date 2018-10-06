90f5ce2c11ed5f0f547d6a54630746a47d825e90
Aphosis <alexy.long@orange.fr>
2018-10-06 03:46:29 +0200

    Added sample spam e-mails on startup.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "raylib.h"
-#include "player.cpp"
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+
+#include "player.cpp"
+#include "mail.cpp"
 
 
 int main() {
@@ -15,6 +17,22 @@ int main() {
     SetTargetFPS(30);
 
     Player player;
+
+    // Initialize the spam e-mails.
+    constexpr unsigned int mailCount = 4;
+    Mail mails[mailCount];
+    mails[0].setTitle("What a deal!");
+    mails[0].setContent("Hesitate no longer, this awesome gaming PC should be yours today!");
+    mails[0].setCategory(Products);
+    mails[1].setTitle("Have you met Ted ?");
+    mails[1].setContent("Lonely, handsome singles in the bar next door!");
+    mails[1].setCategory(Dating);
+    mails[2].setTitle("Cross the street and get a job!");
+    mails[2].setContent("Long past are the days of boredom and poverty, follow my advice and get a job fitting your skills today!");
+    mails[2].setCategory(Jobs);
+    mails[3].setTitle("Don't let the virus spread!");
+    mails[3].setContent("PLEASE READ THIS E-MAIL CAREFULLY!!! The ebola virus is coming back, you MUST take this medecine or risk being exposed and DIE!!!");
+    mails[3].setCategory(HealthCare);
 
     while (!WindowShouldClose() && player.wantedLevel() < 100) {
 
