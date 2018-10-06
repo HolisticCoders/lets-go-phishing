d0cc12a3f6e51781cb33b44a86bc9d9c6e69fcaa
Aphosis <alexy.long@orange.fr>
2018-10-06 02:17:34 +0200

    Added Mail class accessors and mutators.

--- mail.cpp
+++ mail.cpp
@@ -6,12 +6,38 @@ using namespace std;
 
 class Mail {
     public:
+
+        // Constructors
         Mail(const string& title, const string& content, const SpamCategories& category) {
             m_title = title;
             m_content = content;
             m_category = category;
         }
+
+        // Accessors
+        string title() {
+            return m_title;
+        }
+        string content() {
+            return m_content;
+        }
+        SpamCategories category() {
+            return m_category;
+        }
+
+        // Mutators
+        void setTitle(const string& title) {
+            m_title = title;
+        }
+        void setContent(const string& content) {
+            m_content = content;
+        }
+        void setCategory(const SpamCategories& category) {
+            m_category = category;
+        }
+
     private:
+
         string m_title;
         string m_content;
         SpamCategories m_category;
