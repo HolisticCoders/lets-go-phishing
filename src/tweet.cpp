3deac3a313e1ea209aad1cfeeb99a8cf67cff8b5
Aphosis <alexy.long@orange.fr>
2018-10-06 18:11:34 +0200

    Added Tweet class constructors, accessors and mutators.

--- src/tweet.cpp
+++ src/tweet.cpp
@@ -12,12 +12,43 @@ class Victim;
 
 class Tweet {
     public:
+
+        // Constructors
         Tweet() {
-            
+            m_author = nullptr;
+            m_title = "Tweet";
+            m_content = "";
         }
+
+        Tweet(Victim* author, const string& title, const string& content)
+            : m_author{author}, m_title{title}, m_content{content}
+        {}
+
+        // Accessors
+        Victim* author() {
+            return m_author;
+        }
+        string title() {
+            return m_title;
+        }
+        string content() {
+            return m_content;
+        }
+
+        // Mutators
+        void setAuthor(Victim* author) {
+            m_author = author;
+        }
+        void setTitle(const string& title) {
+            m_title = title;
+        }
+        void setContent(const string& content) {
+            m_content = content;
+        }
+
     private:
         Victim* m_author;  // This is made possible by the forward declaration.
         string m_title;
-        string m_body;
+        string m_content;
 };
 
